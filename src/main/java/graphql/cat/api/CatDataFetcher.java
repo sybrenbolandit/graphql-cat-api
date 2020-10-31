6f4ed1a314f1dda96c0a6da5be3e499f874b5011
Sybren Boland <sybren.boland@ns.nl>
2020-10-31 15:44:09 +0100

    Added graphql cat endpoint

---
+++ src/main/java/graphql/cat/api/CatDataFetcher.java
@@ -0,0 +1,15 @@
+package graphql.cat.api;
+
+import graphql.cat.api.domain.CatDTO;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class CatDataFetcher implements DataFetcher<CatDTO> {
+    @Override
+    public CatDTO get(DataFetchingEnvironment dataFetchingEnvironment) {
+        return new CatDTO("Fred", 4);
+    }
+}
---
+++ src/main/java/graphql/cat/api/domain/CatDTO.java
@@ -0,0 +1,29 @@
+package graphql.cat.api.domain;
+
+public class CatDTO {
+
+    private String name;
+    private int age;
+
+    public CatDTO(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
