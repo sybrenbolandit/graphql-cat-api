d7e1d8155023777b635ed335d4084e1a28c105c7
sybrenbolandit <sybrenbolandit@gmail.com>
2021-04-04 22:26:15 +0200

    Add some example configuration

--- src/main/java/graphql/cat/api/logging/LogbookFactory.java
+++ src/main/java/graphql/cat/api/logging/LogbookFactory.java
@@ -5,10 +5,21 @@ import org.zalando.logbook.Logbook;
 
 import javax.inject.Singleton;
 
+import static org.zalando.logbook.Conditions.exclude;
+import static org.zalando.logbook.Conditions.requestTo;
+import static org.zalando.logbook.HeaderFilters.authorization;
+
 @Factory
 public class LogbookFactory {
     @Singleton
     public Logbook logbook() {
-        return Logbook.create();
+        return Logbook.builder()
+                .condition(exclude(requestTo("/health")))
+                //.queryFilter(replaceQuery("password", "<secret>"))
+                .headerFilter(authorization())
+                //.sink(new DefaultSink(
+                //        new SplunkHttpLogFormatter(),
+                //        new DefaultHttpLogWriter()))
+                .build();
     }
 }
